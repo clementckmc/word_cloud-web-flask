6ec19a732fa0438daca757f6bc991661d7ed6eff
clementckmc <46878585+clementckmc@users.noreply.github.com>
2020-12-01 22:57:40 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,111 @@
+import os
+from flask import Flask, flash, jsonify, redirect, render_template, request, send_from_directory, url_for
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.utils import secure_filename
+    
+from generate import apology, wc_txt, wc_doc, wc_pdf
+
+# file uploading
+UPLOAD_FOLDER = "PATH/wordcloud/static/tmp" #TODO
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'doc', 'docx', 'jpeg', 'gif', 'png', 'jpg'}
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# file uploading
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024 
+
+# set files allowed
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+# Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/upload", methods=["GET", "POST"])
+def upload():
+    # upload page
+    if request.method == "GET":
+        return render_template("upload.html")
+    else:
+        # get file
+        if request.files:
+            textfile = request.files["textfile"]
+            mask = request.files["mask"]
+
+            # ensure and secure file
+            if textfile.filename == "":
+                flash("No selected file")
+                return redirect(request.url)
+            
+            if not allowed_file(textfile.filename):
+                flash("file type not allowed")
+                return redirect(request.url)
+            
+            else:
+                filename = secure_filename(textfile.filename)
+                maskname = secure_filename(mask.filename)
+                lang = request.form.get("lang")
+                # without mask
+                if maskname == "":
+                    textfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    return redirect(url_for("uploaded_file", filename=filename, lang=lang))
+                # with mask
+                else:
+                    textfile.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                    mask.save(os.path.join(app.config['UPLOAD_FOLDER'], maskname))
+                    return redirect(url_for("uploaded_file_mask", filename=filename, maskname=maskname, lang=lang))
+
+            
+
+@app.route("/tmp/<filename>?<lang>")
+def uploaded_file(filename, lang):
+    if filename.endswith(".txt"):
+        wc_txt(filename, lang=lang)
+    elif filename.endswith(".doc") or filename.endswith(".docx"):
+        wc_doc(filename, lang=lang)
+    else:
+        wc_pdf(filename, lang=lang)
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    return render_template("output.html", filename=filename)
+
+
+@app.route("/tmp/<filename>?<maskname>?<lang>")
+def uploaded_file_mask(filename, maskname, lang):
+    if filename.endswith(".txt"):
+        wc_txt(filename, maskname, lang)
+    elif filename.endswith(".doc") or filename.endswith(".docx"):
+        wc_doc(filename, maskname, lang)
+    else:
+        wc_pdf(filename, maskname, lang)
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+    os.remove(os.path.join(app.config['UPLOAD_FOLDER'], maskname))
+    return render_template("output.html", filename=filename, maskname=maskname)
+
+
+def errorhandler(e):
+    """Handle error"""
+    if not isinstance(e, HTTPException):
+        e = InternalServerError()
+    return apology(e.name, e.code)
+
+
+if __name__ == '__main__':
+   app.run(debug = True)
---
+++ generate.py
@@ -0,0 +1,167 @@
+import os
+import docx2txt
+import PyPDF2
+import urllib
+import jieba
+import MeCab
+import numpy as np
+from wordcloud import WordCloud, ImageColorGenerator, STOPWORDS
+from PIL import Image
+
+# generate word cloud (without mask)
+def gen_wc(text, lang):
+    if lang == "Jpn":
+        stop_words = ["の", "こと", "年", "月", "日"] + list(STOPWORDS)
+        cloud = WordCloud(font_path="PATH/FONT_YOU_INTENDED_TO_USE", stopwords=stop_words).generate(text) #TODO
+    elif lang == "Chi":
+        stop_words = ["的", "在", "於", "及", "年", "月", "日"] + list(STOPWORDS)
+        cloud = WordCloud(font_path="PATH/FONT_YOU_INTENDED_TO_USE", stopwords=stop_words).generate(text) #TODO
+    else:
+        stop_words = ["said", "will"] + list(STOPWORDS)
+        cloud = WordCloud(stopwords=stop_words).generate(text)
+    os.chdir("PATH/wordcloud/static/tmp") #TODO
+    cloud.to_file("output.png")
+
+# generate word cloud (with mask)
+def gen_wc_m(text, maskname, lang):
+    # get mask
+    mask = np.array(Image.open(os.path.join("PATH/wordcloud/static/tmp", maskname))) #TODO
+
+    # get background color
+    r = mask[0][0][0]
+    g = mask[0][0][1]
+    b = mask[0][0][2]
+
+    # generate word cloud
+    if lang == "Jpn":
+        stop_words = ["の", "こと", "年", "月", "日"] + list(STOPWORDS)
+        cloud = WordCloud(mask=mask, background_color= (r, g, b), stopwords=stop_words, font_path="PATH/FONT_YOU_INTENDED_TO_USE").generate(text) #TODO
+        image_color = ImageColorGenerator(mask)
+        cloud.recolor(color_func= image_color)
+    elif lang == "Chi":
+        stop_words = ["的", "在", "於", "及", "年", "月", "日"] + list(STOPWORDS)
+        cloud = WordCloud(mask=mask, background_color= (r, g, b), font_path="PATH/FONT_YOU_INTENDED_TO_USE", stopwords=stop_words).generate(text) #TODO
+        image_color = ImageColorGenerator(mask)
+        cloud.recolor(color_func= image_color)
+    else:
+        stop_words = ["said", "will"] + list(STOPWORDS)
+        cloud = WordCloud(mask=mask, background_color= (r, g, b), stopwords=stop_words).generate(text)
+        image_color = ImageColorGenerator(mask)
+        cloud.recolor(color_func= image_color)
+    
+    os.chdir("PATH/wordcloud/static/tmp")
+    cloud.to_file("output.png")
+
+
+
+# txt file
+def wc_txt(filename, maskname=None, lang="Eng"):
+    text = open(os.path.join("PATH/wordcloud/static/tmp", filename), "r", encoding="utf-8").read() #TODO
+    if lang == "Chi":
+        text = ' '.join(jieba.cut(text))
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+    
+    elif lang == "Jpn":
+        # MeCabの準備
+        tagger = MeCab.Tagger("-Owakati")
+        tagger.parse(text)
+        node = tagger.parseToNode(text)
+
+        # 名詞を取り出す
+        word_list = []
+        while node:
+            word_type = node.feature.split(',')[0]
+            if word_type == '名詞':
+                word_list.append(node.surface)
+            node = node.next
+
+        # リストを文字列に変換
+        word_chain = ' '.join(word_list)
+
+        if maskname == None:
+            gen_wc(word_chain, lang)
+        else:
+            gen_wc_m(word_chain, maskname, lang)
+    
+    # English
+    else:
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+
+
+
+# docx file
+def wc_doc(filename, maskname=None, lang="Eng"):
+    text = docx2txt.process(os.path.join("PATH/wordcloud/static/tmp", filename)) #TODO
+    if lang == "Chi":
+        text = ' '.join(jieba.cut(text))
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+    elif lang == "Jpn":
+        tagger = MeCab.Tagger("-Owakati")
+        tagger.parse(text)
+        node = tagger.parseToNode(text)
+        word_list = []
+        while node:
+            word_type = node.feature.split(',')[0]
+            if word_type == '名詞':
+                word_list.append(node.surface)
+            node = node.next
+        word_chain = ' '.join(word_list)
+        if maskname == None:
+            gen_wc(word_chain, lang)
+        else:
+            gen_wc_m(word_chain, maskname, lang)
+    else:
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+
+
+
+# pdf file
+def wc_pdf(filename, maskname=None, lang="Eng"):
+    File = open(os.path.join("PATH/wordcloud/static/tmp", filename),'rb') #TODO
+    pdf = PyPDF2.PdfFileReader(File)
+    textlst = []
+    for page in pdf.pages:
+        textlst.append(page.extractText())
+    text = " ".join(textlst)
+    if lang == "Chi":
+        text = ' '.join(jieba.cut(text))
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+    elif lang == "Jpn":
+        tagger = MeCab.Tagger("-Owakati")
+        tagger.parse(text)
+        node = tagger.parseToNode(text)
+        word_list = []
+        while node:
+            word_type = node.feature.split(',')[0]
+            if word_type == '名詞':
+                word_list.append(node.surface)
+            node = node.next
+        word_chain = ' '.join(word_list)
+        if maskname == None:
+            gen_wc(word_chain, lang)
+        else:
+            gen_wc_m(word_chain, maskname, lang)
+    else:
+        if maskname == None:
+            gen_wc(text, lang)
+        else:
+            gen_wc_m(text, maskname, lang)
+
+
+def apology(message, code=400):
+    print(message)
